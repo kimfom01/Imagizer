8bc59735a293a68a05294b5671e7f8d265aff74d
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-03-16 11:15:54 +0300

    Implement resize request tests

---
+++ Imagizer.Tests/ResizeTests.cs
@@ -0,0 +1,65 @@
+using System.Net;
+
+namespace Imagizer.Tests;
+
+public class ResizeTests : IDisposable
+{
+    private readonly ImagizerApiFactory _api;
+    private readonly HttpClient _httpClient;
+    private readonly Stream _stream;
+    private readonly StreamContent _fileContent;
+
+    public ResizeTests()
+    {
+        _api = new ImagizerApiFactory();
+        _httpClient = _api.CreateClient();
+        _stream = File.OpenRead("./Assets/wallpaper.jpg");
+        _fileContent = new StreamContent(_stream);
+    }
+
+    public void Dispose()
+    {
+        _api.Dispose();
+        _httpClient.Dispose();
+        _stream.Dispose();
+        _fileContent.Dispose();
+    }
+
+    [Fact]
+    public async Task ResizeRequestShouldReturnBadRequestWhenSizeMissing()
+    {
+        using var resizeRequest = new MultipartFormDataContent();
+
+        resizeRequest.Add(_fileContent, "ImageFile", "wallpaper.jpg");
+
+        var result = await _httpClient.PostAsync("/api/Image/resize", resizeRequest);
+
+        Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task ResizeRequestShouldReturnBadRequestWhenFormFileMissing()
+    {
+        using var resizeRequest = new MultipartFormDataContent();
+        using var sizeContent = new StringContent(Convert.ToString(400));
+        resizeRequest.Add(sizeContent, "Size");
+
+        var result = await _httpClient.PostAsync("/api/Image/resize", resizeRequest);
+
+        Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task ResizeRequestShouldReturn200WhenImageAndSizeCorrect()
+    {
+        using var sizeContent = new StringContent(Convert.ToString(400));
+        using var convertRequest = new MultipartFormDataContent();
+
+        convertRequest.Add(_fileContent, "ImageFile", "wallpaper.jpg");
+        convertRequest.Add(sizeContent, "Size");
+
+        var result = await _httpClient.PostAsync("/api/Image/resize", convertRequest);
+
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+    }
+}
