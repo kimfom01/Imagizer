a2f850c815c32209a44bf8edc46b1de7d52ff717
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-03-16 13:18:14 +0300

    Clean up Program.cs

--- Imagizer.Api/Program.cs
+++ Imagizer.Api/Program.cs
@@ -1,56 +1,8 @@
-using System.Reflection;
-using System.Threading.RateLimiting;
-using Imagizer.Api.Infrastructure.LinkShortener;
-using Imagizer.Api.Services;
 using Imagizer.Api.Utils;
-using Microsoft.OpenApi.Models;
-using Unchase.Swashbuckle.AspNetCore.Extensions.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Imagizer API",
-        Description = "A Web API for manipulating images based on the Magick.NET library",
-        Contact = new OpenApiContact
-        {
-            Name = "Kim Fom",
-            Email = "kimfom01@gmail.com",
-            Url = new Uri("https://kimfom.space")
-        },
-        License = new OpenApiLicense
-        {
-            Name = "MIT Licence",
-            Url = new Uri("https://opensource.org/license/mit/")
-        }
-    });
-
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
-builder.Services.ConfigureSwaggerGen(options => { options.AddEnumsWithValuesFixFilters(); });
-builder.Services.AddScoped<IImageProcessorService, ImageProcessorService>();
-builder.Services.AddRateLimiter(options =>
-{
-    options.AddPolicy("fixed-by-ip", httpContext =>
-    {
-        return RateLimitPartition.GetFixedWindowLimiter(
-            partitionKey: httpContext.Connection.RemoteIpAddress?.ToString(),
-            factory: _ => new FixedWindowRateLimiterOptions
-            {
-                PermitLimit = 10,
-                Window = TimeSpan.FromMinutes(1)
-            });
-    });
-});
-builder.Services.AddHttpClient<IUrlShortener, UrlShortener>(
-    opt => { opt.BaseAddress = new Uri(ConfigHelper.GetVariable("SHORTENER:API_HOST", builder.Configuration)); });
-builder.Services.AddScoped<IUrlShortener, UrlShortener>();
+builder.Services.RegisterServices(builder.Configuration);
 
 var app = builder.Build();
 
